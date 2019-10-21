57c28966c8cdd063255cca448981d2873c4b618b
Vladimír Vondruš <mosra@centrum.cz>
2019-10-21 16:20:20 +0200

    Clean up the SDL template, too.

--- test/template_sdl/flextGL.c
+++ test/template_sdl/flextGL.c
@@ -7,102 +7,49 @@
 */
 
 #include "flextGL.h"
-#include <SDL.h>
 
+#include <SDL.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+void flextLoadOpenGLFunctions(void) {
+    /* GL_ARB_tessellation_shader */
+    glpfPatchParameteri = (PFNGLPATCHPARAMETERI_PROC*)SDL_GL_GetProcAddress("glPatchParameteri");
 
-void flextLoadOpenGLFunctions(void);
+    /* GL_VERSION_3_0 */
+    glpfGetStringi = (PFNGLGETSTRINGI_PROC*)SDL_GL_GetProcAddress("glGetStringi");
 
-/**
- * \return -1 on error, 0 on success
- * \sa SDL_GetError
- */
-int flextInit(void)
-{
+}
+
+int flextInit(void) {
     int major;
     int minor;
-
     SDL_GL_GetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, &major);
     SDL_GL_GetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, &minor);
 
     flextLoadOpenGLFunctions();
 
-    /* --- Check for minimal version and profile --- */
+    /* Check for minimal version and profile */
 
-    if (major * 10 + minor < 32) {
+    if(major * 10 + minor < 32) {
         return SDL_SetError("OpenGL context hasn't the expected version 3.2.");
     }
 
-
     /* --- Check for extensions --- */
 
-    if (SDL_GL_ExtensionSupported("GL_ARB_tessellation_shader")) {
+    if(SDL_GL_ExtensionSupported("GL_ARB_tessellation_shader")) {
         FLEXT_ARB_tessellation_shader = GL_TRUE;
     }
-
 
     return 0;
 }
 
-
-
-void flextLoadOpenGLFunctions(void)
-{
-    /* --- Function pointer loading --- */
-
-
-    /* GL_ARB_tessellation_shader */
-
-    glpfPatchParameteri = (PFNGLPATCHPARAMETERI_PROC*)SDL_GL_GetProcAddress("glPatchParameteri");
-
-
-    /* GL_VERSION_1_2 */
-
-
-
-    /* GL_VERSION_1_3 */
-
-
-
-    /* GL_VERSION_1_4 */
-
-
-
-    /* GL_VERSION_1_5 */
-
-
-
-    /* GL_VERSION_2_0 */
-
-
-
-    /* GL_VERSION_2_1 */
-
-
-
-    /* GL_VERSION_3_0 */
-
-    glpfGetStringi = (PFNGLGETSTRINGI_PROC*)SDL_GL_GetProcAddress("glGetStringi");
-
-
-    /* GL_VERSION_3_1 */
-
-
-
-    /* GL_VERSION_3_2 */
-
-
-
-}
-
-/* ----------------------- Extension flag definitions ---------------------- */
+/* Extension flag definitions */
 int FLEXT_ARB_tessellation_shader = GL_FALSE;
 
-/* ---------------------- Function pointer definitions --------------------- */
+/* Function pointer definitions */
 
 /* GL_ARB_tessellation_shader */
 
@@ -111,8 +58,6 @@ PFNGLPATCHPARAMETERI_PROC* glpfPatchParameteri = NULL;
 /* GL_VERSION_3_0 */
 
 PFNGLGETSTRINGI_PROC* glpfGetStringi = NULL;
-
-
 
 #ifdef __cplusplus
 }
--- test/template_sdl/flextGL.h
+++ test/template_sdl/flextGL.h
@@ -34,7 +34,7 @@ extern "C" {
 #define GLAPI extern
 #endif
 
-/* -------------------------------- DATA TYPES ------------------------------- */
+/* ------------------------------- DATA TYPES ------------------------------ */
 
 typedef unsigned int GLenum;
 typedef void GLvoid;
@@ -44,7 +44,7 @@ typedef unsigned int GLuint;
 typedef int GLsizei;
 typedef khronos_uint16_t GLhalf;
 
-/* ----------------------------------- ENUMS --------------------------------- */
+/* ---------------------------------- ENUMS -------------------------------- */
 
 /* GL_VERSION_1_0 */
 
@@ -921,8 +921,7 @@ typedef khronos_uint16_t GLhalf;
 #define GL_TESS_EVALUATION_SHADER 0x8E87
 #define GL_TESS_CONTROL_SHADER 0x8E88
 
-/* --------------------------- FUNCTION PROTOTYPES --------------------------- */
-
+/* -------------------------- FUNCTION PROTOTYPES -------------------------- */
 
 /* GL_ARB_tessellation_shader */
 
@@ -932,52 +931,13 @@ GLAPI PFNGLPATCHPARAMETERI_PROC* glpfPatchParameteri;
 
 #define glPatchParameteri glpfPatchParameteri
 
-
 /* GL_VERSION_1_0 */
 
 GLAPI void APIENTRY glGetIntegerv (GLenum pname, GLint * data);
 
-
 /* GL_VERSION_1_1 */
 
 GLAPI void APIENTRY glDrawElements (GLenum mode, GLsizei count, GLenum type, const void * indices);
-
-
-/* GL_VERSION_1_2 */
-
-
-
-
-
-/* GL_VERSION_1_3 */
-
-
-
-
-
-/* GL_VERSION_1_4 */
-
-
-
-
-
-/* GL_VERSION_1_5 */
-
-
-
-
-
-/* GL_VERSION_2_0 */
-
-
-
-
-
-/* GL_VERSION_2_1 */
-
-
-
-
 
 /* GL_VERSION_3_0 */
 
@@ -987,20 +947,7 @@ GLAPI PFNGLGETSTRINGI_PROC* glpfGetStringi;
 
 #define glGetStringi glpfGetStringi
 
-
-/* GL_VERSION_3_1 */
-
-
-
-
-
-/* GL_VERSION_3_2 */
-
-
-
-
-
-/* --------------------------- CATEGORY DEFINES ------------------------------ */
+/* -------------------------- CATEGORY DEFINES ----------------------------- */
 
 #define GL_ARB_tessellation_shader
 #define GL_VERSION_1_0
@@ -1015,8 +962,7 @@ GLAPI PFNGLGETSTRINGI_PROC* glpfGetStringi;
 #define GL_VERSION_3_1
 #define GL_VERSION_3_2
 
-/* ---------------------- Flags for optional extensions ---------------------- */
-
+/* --------------------- FLAGS FOR OPTIONAL EXTENSIONS --------------------- */
 
 extern int FLEXT_ARB_tessellation_shader;
 
@@ -1030,4 +976,4 @@ int flextInit(void);
 }
 #endif
 
-#endif /* _gl_h_ */
+#endif
